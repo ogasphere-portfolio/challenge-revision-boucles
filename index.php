8d72ff62f201efdfa5d5c855d9b9e363d19c9b35
Olivier <olivier@ogasphere.com>
2021-07-13 10:48:07 +0200

    Terminé

--- index.php
+++ index.php
@@ -12,15 +12,41 @@
       <div class="boxes">
         <?php
         // TODO
+       
+        // On crée un tableau vide qui viendra accueillir nos valeurs.
+        $arrayHexas = [];
+        for ($index=0; $index < 16; $index++) { 
+          // On convertindext le compteur $index en hexadécindexmal et on l'ajoute dans une nouvelle entrée du tableau.  
+          $arrayHexas[] = dechex($index);
+        }
+        
+        foreach($arrayHexas as $arrayHexa){
+          $colorHex = '#'. $arrayHexa . $arrayHexa . $arrayHexa;
+          echo '<div class="box" style="background-color:'.$colorHex .'"></div>';
+        }
+        unset($arrayHexa);
+        
+     
+
+
         ?>
       </div>
-      <!--<h2>Toutes les couleurs</h2>
+      <h2>Toutes les couleurs</h2>
       <div class="boxes">
         <?php
         // TODO
+        foreach ($arrayHexas as $red) {
+          foreach ($arrayHexas as $green) {
+            foreach ($arrayHexas as $blue) {
+              $colorHex = '#'. $red . $green . $blue;
+              echo '<div class="box" style="background-color:'.$colorHex .'"></div>';
+
+            }
+          }
+        }
         ?>
       </div>
-      -->
+     
     </div>
 	</body>
 </html>
